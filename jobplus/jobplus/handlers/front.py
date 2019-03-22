cb9b42e73244dcfc1e5e56f8e73c8b8c10f23121
shixuezhe <1159513993@qq.com>
2019-03-22 10:05:56 +0800

    mysite code

--- jobplus/jobplus/handlers/front.py
+++ jobplus/jobplus/handlers/front.py
@@ -3,9 +3,21 @@ from flask import current_app,request
 from jobplus.models import User,Company,Job
 from jobplus.forms import User_RegisterForm,Company_RegisterForm,LoginForm,Phone_RegisterForm
 from flask_login import login_user,logout_user,login_required
-from jobplus.send_code import send_code
+import random
+import jobplus.zhenzismsclient as smsclient
 
 front=Blueprint('front',__name__)
+'''
+发送验证码函数
+'''
+def send_code(number):
+    code = ''
+    for i in range(1, 5):
+        code = code + str(random.randint(1, 9))
+    client = smsclient.ZhenziSmsClient('https://sms_developer.zhenzikj.com','101043', 'MGVjY2FhMzEtZDE3NC00MDA3LWE1NDItNWQwMWQwNmEyYTE4')
+    result = client.send(number, '您的验证码为' + code + ',请不要将验证码泄露给他人哦！！！')
+    return code
+
 
 @front.route('/')
 def index():
@@ -26,25 +38,16 @@ def userregister():
     return render_template('user_register.html',form=form,active='email')
 
 @front.route('/phoneregister',methods=['GET','POST'])
-def phoneregister(request):
+def phoneregister():
     form = Phone_RegisterForm()
-    if request.POST:
-        if "code" in request.POST:
-            number = send_code(form.phone.data)
-            code = int(number)
-            flash('验证码发送成功，请及时查收' + number, 'success')
-            return code
-        if form.validate_on_submit():
-            if form.code.data:
-                if form.code.data == code:
-                    form.create_user()
-                    flash('注册成功，请登录','success')
-                    return redirect(url_for('front.login'))
-                else:
-                    flash('验证码错误','success')
-            else:
-                flash('请输入验证码')
-    return render_template('phone_register.html',form=form,active='phone')
+    if form.validate_on_submit():
+        if form.code.data:
+            form.create_user()
+            flash('注册成功，请登录','success')
+            return redirect(url_for('front.login'))
+        else:
+            flash('验请输入证码','success')
+    return render_template('phone_register.html',form=form,active='phone',send_code=send_code)
 
 @front.route('/companyregister',methods=['GET','POST'])
 def companyregister():
